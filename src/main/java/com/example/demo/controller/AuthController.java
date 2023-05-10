38210ffe5e330f48de8d9660eb820d17b07f8ded
Zxiyue <zc17838590509@163.com>
2023-05-10 23:02:05 +0800

    erupt

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * @program: erupt-example
+ * @description: 用于注册用户
+ *
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/erupt-api/anth")
+public class AuthController {
+
+}
